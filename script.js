aa3ad91f5b1b11db6bd416f1c48447d8198dec59
joselmm <95531879+joselmm@users.noreply.github.com>
2023-04-30 10:58:52 -0500

    Create script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ socket.onmessage = function(event) {
   var jsonOb = JSON.parse(event.data);
   $respuesta.value = jsonOb.answer;
   enviando=false;
-  if(jsonObj.state="complete"){$btnSend.disabled = false;}
+  if(jsonOb.state="complete"){$btnSend.disabled = false;}
 };
 
 socket.onclose = function(event) {
